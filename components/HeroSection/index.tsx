f5ed0c802d158bf05912a15f0aef9fe12a6fb753
Pilyaev Vadim <dr.pilyaev@yandex.ru>
2022-03-29 13:37:23 +0300

    fix home HeroSection headings for mobile

--- components/HeroSection/index.tsx
+++ components/HeroSection/index.tsx
@@ -1,44 +1,41 @@
 import * as React from 'react';
-import { Container, Typography } from '@mui/material';
+import { Container, Typography, useTheme } from '@mui/material';
 
 import { Search } from '@features/Search';
 
 import styles from './HeroSection.module.css';
+import { Box } from '@material-ui/core';
 
-const containerStyles = { // TODO: better variant?
-  maxWidth: '480px',
-  '@media(minWidth: 1200px)' : {
-    maxWidth: 'none',
-  }
-};
 
 export const HeroSection = (): React.ReactElement => {
   const title = 'Сервис краткосрочной аренды жилья';
   const description = 'HeartApart объединяет гостей и хозяев уникальных квартир';
 
+  const theme = useTheme();
+
   return (
-    <div className={styles.host}>
-      <Container sx={containerStyles}>
-        <Typography
-          variant='h3'
-          component='div'
-          gutterBottom
-          sx={{ fontWeight: 700 }}
-          className={styles.title}
-        >
-          {title}
-        </Typography>
-        <Typography
-          variant='h5'
-          component='div'
-          gutterBottom
-          sx={{ fontWeight: 700 }}
-          className={styles.description}
-        >
-          {description}
-        </Typography>
-        <Search />
+    <Box className={styles.host} sx={{padding:{xs:'50px 0', md:'150px 0',}}}>
+      <Container>
+        <Box sx={{maxWidth:{ xs:'480px', lg:'none' }, margin:'0 auto'}} >
+          <Typography
+            component='div'
+            gutterBottom
+            sx={{ fontWeight:700, fontSize:{ xs:theme.typography.h4.fontSize, lg:theme.typography.h3.fontSize} }}
+            className={styles.title}
+          >
+            {title}
+          </Typography>
+          <Typography
+            component='div'
+            gutterBottom
+            sx={{ fontSize:{ xs:theme.typography.body1.fontSize, lg:theme.typography.subtitle1.fontSize} }}
+            className={styles.description}
+          >
+            {description}
+          </Typography>
+          <Search />
+        </Box>
       </Container>
-    </div>
+    </Box>
   );
 };
