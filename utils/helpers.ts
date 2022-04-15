76651f73210a107dc7c2a21aa840e0b9936175c2
Pilyaev Vadim <dr.pilyaev@yandex.ru>
2022-04-15 19:17:00 +0300

    add isDateInArray helper

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,3 +1,15 @@
 export const addZeroBefore = (n) => {
   return (n < 10 ? '0' : '') + n;
 };
+
+export function isDateInArray(array, dateString) {
+  let dateExists = false;
+  let date = new Date(dateString);
+  array.forEach(function(arrayDateString) {
+    let arrayDate = new Date(arrayDateString);
+    if(date.getTime() === arrayDate.getTime()){
+      dateExists = true;
+    }
+  });
+  return dateExists;
+}
