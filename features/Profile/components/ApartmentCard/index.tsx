ec5f450967cac506f33e64c114bb7280efd3024b
Pilyaev Vadim <dr.pilyaev@yandex.ru>
2022-04-29 18:20:41 +0300

    add badges to profile ApartmentCard

--- features/Profile/components/ApartmentCard/index.tsx
+++ features/Profile/components/ApartmentCard/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import Link from 'next/link';
 
-import {Box, Card, CardContent, CardMedia,  Stack, Typography} from '@mui/material';
+import {Box, Card, CardContent, CardMedia, Chip,  Stack, Typography} from '@mui/material';
 
 import StarIcon from "@mui/icons-material/Star";
 
@@ -10,14 +10,32 @@ type TProfileApartmentProps = {
 	apartment:any
 };
 
-export default function ApartmentCard({apartment:{publicInfo, externalRating, images}}: TProfileApartmentProps) {
+export default function ApartmentCard({ apartment }:TProfileApartmentProps) {
+
+	const {publicInfo, externalRating, images, status} = apartment;
+
 	return (
 		<Card>
-			{ (images && images.length !== 0) ? (
-				<CardMedia component="img" height="140" image={images[0].imageUrl} alt={''}/>
-			):(
-				<Box sx={{height:'140px', background:'#707070'}}/>
-			)}
+			<Box sx={{ position:'relative', height:'140px', background:'#707070', zIndex:0, p:1.5 }}>
+				{ (images && images.length !== 0) && (
+					<CardMedia component="img" height="140" image={images[0].imageUrl} alt={''} sx={{position:'absolute', left:0, top:0,zIndex:-1}}/>
+				)}
+				{ externalRating?.isLuxury && <Chip label="Премиум" color="success" size="small"/>}
+				{ externalRating?.superHost && <Chip label="Топ-сервис" color="secondary" size="small" />}
+				{ status === 'CREATED' && (
+					<Box sx={{
+							position:'absolute', left:'50%', top:'50%',
+							transform: 'translate3d(-50%, -50%, 0)',
+							background: 'rgba(0, 0, 0, 0.17)',
+							backdropFilter: 'blur(15px)',
+							borderRadius: '4px',
+							py:1,
+							px:3,
+							color: '#ffffff',
+						}}><Typography variant="body1">Черновик</Typography>
+					</Box>
+				)}
+			</Box>
 			<CardContent>
 				<Typography variant="h5">{publicInfo.name}</Typography>
 				<Stack direction="row" alignItems="center" sx={{mb: 0.5}}>
