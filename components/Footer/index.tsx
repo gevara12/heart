d5888801213bb11ef2ad86079860203b777531ba
Mangrayd <VadimFrontend@yandex.ru>
2022-05-26 03:40:42 +0300

    update footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -2,13 +2,16 @@ import * as React from 'react';
 import Link from 'next/link';
 
 import { Box, Container, Stack, Typography, useTheme, useMediaQuery, Divider } from '@mui/material';
-import { ThemeSwitcher } from '@components/ThemeSwitcher';
+
 
 export const Footer = (): React.ReactElement => {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+  const isTablet = useMediaQuery(theme.breakpoints.between('sm','md'));
+
   const supportLink = 'support@heartapart.ru';
   const feedbackLink = 'feedback@heartapart.ru';
+
   const navigation = [
     { url: '/terms', title: 'Условия' },
     { url: '/privacy', title: 'Политика конфиденциальности' },
@@ -22,35 +25,46 @@ export const Footer = (): React.ReactElement => {
 
   return (
     <footer>
-      <Box sx={{ py: 5, bgcolor: 'background.paper' }}>
+      <Box sx={{ py: isMobile ? 4 : 5, bgcolor: 'background.paper' }}>
         <Container maxWidth="lg">
-          <Typography variant="h3" sx={{ mb: 5 }}>
-            HeartApart
-          </Typography>
-          <Stack spacing={isMobile ? 1 : 10} direction={isMobile ? 'column' : 'row'}>
+          <Stack direction={'row'} alignItems={'center'} sx={{mb:isMobile?4.5:6.5}}>
+            <Box>
+              <Link href='/hero' passHref>
+                <a style={{ display: 'block' }}>
+                  <img style={{ display: 'block', height: isMobile ? '34px' : '44px' }} src={'/images/heart-logo.png'} alt={''} />
+                </a>
+              </Link>
+            </Box>
+
+            <Typography variant={'h4'} sx={{ml:isMobile ? 1.5 : (isTablet ? 2 : 2.5), fontWeight:500}}>Heartapart</Typography>
+            <Typography variant={'h4'} sx={{ml:2.5, px:1 ,border:'1px solid', borderColor:'background',borderRadius: '4px', fontWeight:500}}>beta</Typography>
+
+          </Stack>
+
+          <Stack spacing={isMobile ? 2.5 : (isTablet? 10.5 : 12.5)} direction={isMobile ? 'column' : 'row'}>
             <Link href={`mailto:${supportLink}`} passHref>
-              <a target="_blank" rel="noopener noreferrer" style={{ color: theme.palette.text.primary }}>
-                Написать в поддержку
+              <a target="_blank" rel="noopener noreferrer" style={{ color: theme.palette.text.primary, textDecoration: 'none' }}>
+                <Typography variant={'body1'} component={'div'} sx={{}}>Написать в поддержку</Typography>
               </a>
             </Link>
             <Link href={`mailto:${feedbackLink}`} passHref>
-              <a target="_blank" rel="noopener noreferrer" style={{ color: theme.palette.text.primary }}>
-                Обратная связь
+              <a target="_blank" rel="noopener noreferrer" style={{ color: theme.palette.text.primary, textDecoration: 'none' }}>
+                <Typography variant={'body1'} component={'div'} sx={{}}>Обратная связь</Typography>
               </a>
             </Link>
           </Stack>
-          <Divider sx={{ mt: 5, mb: 4 }} />
-          <Stack spacing={isMobile ? 1 : 10} direction={isMobile ? 'column' : 'row'}>
+
+          <Divider sx={{ mt: isMobile? 4:7, mb: isMobile? 2.5:5 }} />
+
+          <Stack spacing={isMobile ? 2.5: (isTablet? 4:5)} direction={isMobile ? 'column' : 'row'}>
             {navigation.map((navLink, i) => (
               <Link href={navLink.url} passHref key={i}>
-                <a style={{ color: theme.palette.text.primary, textDecoration: 'none' }}>{navLink.title}</a>
+                <a style={{ color:theme.palette.text.secondary, textDecoration: 'none' }}>
+                  <Typography variant={isMobile?'caption':'body1'} component={'div'} sx={{}}>{navLink.title}</Typography>
+                </a>
               </Link>
             ))}
-          </Stack>
-          <Stack direction="row" alignItems="center" justifyContent="space-between" sx={{ mt: 4 }}>
-            <span>© {year}, HeartApart</span>
-
-            <ThemeSwitcher />
+            <Typography variant={isMobile?'caption':'body1'} component={'div'} sx={{ color: theme.palette.text.secondary, marginLeft:isMobile ? 0: 'auto' }}>© {year}, HeartApart</Typography>
           </Stack>
         </Container>
       </Box>
